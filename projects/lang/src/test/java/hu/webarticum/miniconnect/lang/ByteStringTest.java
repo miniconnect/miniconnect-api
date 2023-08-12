d63833ab3c05bec499f3510091299fc84b0fc062
Dávid Horváth <horvath@webarticum.hu>
2023-08-12 05:07:45 +0200

    Add more tests to ByteStringTest

--- projects/lang/src/test/java/hu/webarticum/miniconnect/lang/ByteStringTest.java
+++ projects/lang/src/test/java/hu/webarticum/miniconnect/lang/ByteStringTest.java
@@ -1,6 +1,10 @@
 package hu.webarticum.miniconnect.lang;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +13,114 @@ class ByteStringTest {
     @Test
     void testEmpty() {
         ByteString emptyByteString = ByteString.empty();
+        assertThat(emptyByteString.isEmpty()).isTrue();
         assertThat(emptyByteString.length()).isZero();
+        assertThat(emptyByteString.extract()).isEmpty();
+        assertThat(emptyByteString.iterator()).isExhausted();
+        assertThatThrownBy(() -> emptyByteString.extract(10, 20)).isInstanceOf(IndexOutOfBoundsException.class);
     }
     
     @Test
-    void test() {
+    void testCreators() {
+        assertThat(ByteString.of(new byte[] { 108, 111, 114, 101, 109 }).extract())
+                .containsExactly(108, 111, 114, 101, 109);
+        assertThat(ByteString.of(new byte[] { 108, 111, 114, 101, 109 }, 1, 2).extract())
+                .containsExactly(111, 114);
+        assertThat(ByteString.of("lorem").extract())
+                .containsExactly(108, 111, 114, 101, 109);
+        assertThat(ByteString.of("lorem", StandardCharsets.US_ASCII).extract())
+                .containsExactly(108, 111, 114, 101, 109);
+        assertThat(ByteString.ofByte((byte) 123).extract())
+                .containsExactly(123);
+        assertThat(ByteString.ofChar('r').extract())
+                .containsExactly(0, 114);
+        assertThat(ByteString.ofShort((short) 123).extract())
+                .containsExactly(0, 123);
+        assertThat(ByteString.ofInt(123).extract())
+                .containsExactly(0, 0, 0, 123);
+        assertThat(ByteString.ofLong(123L).extract())
+                .containsExactly(0, 0, 0, 0, 0, 0, 0, 123);
+        assertThat(ByteString.ofFloat(0.5f).extract())
+                .containsExactly(63, 0, 0, 0);
+        assertThat(ByteString.ofDouble(0.5).extract())
+                .containsExactly(63, -32, 0, 0, 0, 0, 0, 0);
+        assertThat(ByteString.fromInputStream(new ByteArrayInputStream(
+                "lorem ipsum".getBytes(StandardCharsets.UTF_8))).extract())
+                .containsExactly(108, 111, 114, 101, 109, 32, 105, 112, 115, 117, 109);
+        assertThat(ByteString.fromInputStream(new ByteArrayInputStream(
+                "lorem ipsum".getBytes(StandardCharsets.UTF_8)), 11).extract())
+                .containsExactly(108, 111, 114, 101, 109, 32, 105, 112, 115, 117, 109);
+    }
 
+    @Test
+    void testBuilderEmpty() {
+        ByteString.Builder builder = ByteString.builder();
+        assertThat(builder.length()).isZero();
+        assertThat(builder.build()).isEmpty();
+    }
+
+    @Test
+    void testBuilderSingle() {
+        ByteString.Builder builder = ByteString.builder().append(ByteString.of("abc"));
+        assertThat(builder.length()).isEqualTo(3);
+        assertThat(builder.build()).map(b -> (int) b).containsExactly(97, 98, 99);
+    }
+    
+    @Test
+    void testBuilderComplex() {
+        ByteString.Builder builder = ByteString.builder()
+                .append(ByteString.of("lorem"))
+                .append(ByteString.of("lorem"), 1, 2)
+                .append((byte) 45)
+                .append(new byte[] { 7, -12, 84, 101, 3, 7, 0, 99, 23 }, 3, 4)
+                .append(new byte[] { 7, -12 })
+                .appendChar('r')
+                .appendShort((short) 123)
+                .appendInt(234)
+                .appendLong(456L)
+                .appendFloat(0.25f)
+                .appendDouble(0.75);
+        assertThat(builder.length()).isEqualTo(42);
+        assertThat(builder.build()).map(b -> (int) b).containsExactly(
+                108, 111, 114, 101, 109, 111, 114, 45, 101, 3, 7, 0, 7, -12, 0, 114, 0, 123, 0, 0, 0, -22,
+                0, 0, 0, 0, 0, 0, 1, -56, 62, -128, 0, 0, 63, -24, 0, 0, 0, 0, 0, 0);
+    }
+
+    @Test
+    void testByteAt() {
+        ByteString byteString = ByteString.of("lorem");
+        assertThatThrownBy(() -> byteString.byteAt(10)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThat(byteString.byteAt(2)).isEqualTo((byte) 114);
+    }
+
+    @Test
+    void testSubstring() {
+        ByteString byteString = ByteString.of("lorem");
+        assertThatThrownBy(() -> byteString.substring(9)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThat(byteString.substring(3)).isEqualTo(ByteString.of("em"));
+    }
+
+    @Test
+    void testSubstringUntil() {
+        ByteString byteString = ByteString.of("lorem");
+        assertThatThrownBy(() -> byteString.substring(2, 10)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThat(byteString.substring(1, 3)).isEqualTo(ByteString.of("or"));
+    }
+
+    @Test
+    void testSubstringLength() {
+        ByteString byteString = ByteString.of("lorem");
+        assertThatThrownBy(() -> byteString.substringLength(3, 5)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThat(byteString.substringLength(2, 3)).isEqualTo(ByteString.of("rem"));
+        assertThat(byteString.substringLength(0, 5)).isSameAs(byteString);
+    }
+
+    @Test
+    void test() {
+        
         // TODO
         //assertThat(true).isFalse();
         
     }
-
+    
 }
