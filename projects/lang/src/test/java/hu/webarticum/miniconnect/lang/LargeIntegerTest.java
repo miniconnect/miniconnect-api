7010e80e672cd2a2cc8bd00515cab1132cd133d5
Dávid Horváth <horvath@webarticum.hu>
2023-08-16 04:28:14 +0200

    Prepare LargeIntegerTest

--- projects/lang/src/test/java/hu/webarticum/miniconnect/lang/LargeIntegerTest.java
+++ projects/lang/src/test/java/hu/webarticum/miniconnect/lang/LargeIntegerTest.java
@@ -3,8 +3,21 @@ package hu.webarticum.miniconnect.lang;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class LargeIntegerTest {
+    
+    private static final String DATA_DIR = "/hu/webarticum/miniconnect/lang/test-cases/ImmutableMapTest";
+    
+    
+    // TODO
+    
+    @ParameterizedTest
+    @CsvFileSource(resources = DATA_DIR + "/add-cases.csv", numLinesToSkip = 1)
+    void testAdd(LargeInteger p1, LargeInteger p2, LargeInteger p3) {
+        assertThat(p1.add(p2)).isEqualTo(p3);
+    }
     
     @Test
     void test() {
