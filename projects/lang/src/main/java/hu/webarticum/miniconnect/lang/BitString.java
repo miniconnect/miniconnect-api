984e123c5e44146ef37da60c49d798b742a07140
Dávid Horváth <horvath@webarticum.hu>
2026-03-14 01:50:29 +0100

    Remove unused import

--- projects/lang/src/main/java/hu/webarticum/miniconnect/lang/BitString.java
+++ projects/lang/src/main/java/hu/webarticum/miniconnect/lang/BitString.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 /**
  * Simple immutable bit array implementation.
